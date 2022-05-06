d928587e224c8db54eb4d1eb6ccd316dd93219cf
Somewell <samvel121202@mail.ru>
2022-05-06 14:05:26 +0300

    Added a project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+import router from "@/router/router";
+import store from "@/data/store/vuex";
+
+import directives from "@/services/directives/directives";
+
+const app = createApp(App);
+
+directives.forEach((directive) => {
+  app.directive(directive.name, directive);
+});
+
+app.use(store).use(router).mount("#app");
